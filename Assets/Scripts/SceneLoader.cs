79dcdaabdd161ffbb28cc7b2bee0ca2c5f9a8d75
TerminalSniper5 <00loufra55@gmail.com>
2023-11-10 12:51:43 +1030

    death, we die

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -7,12 +7,31 @@ public class SceneLoader : MonoBehaviour
 {
     public AudioSource click;
 
+    public Collider monster;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Main Menu");
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        //if(SceneManager.Equals("")) check current scene, if death, start coroutine, wait for length of death cutscene clip then transition to main menu
+    }
+
+    private void OnCollisionEnter(Collision monster)
+    {
+        SceneManager.LoadScene("Death");
+    }
+
     public void PlayGame()
     {
         click.Play();
         StartCoroutine(AudioWait(click));
         SceneManager.LoadScene("Greybox");
-        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     public void QuitGame()
